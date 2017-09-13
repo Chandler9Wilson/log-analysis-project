4f8286868ca0b52acddbf520485a153fab196b10
Chandler Wilson <chandler9wilson@gmail.com>
2017-09-13 10:54:18 -0500

    interactive() working

--- logAnalysis.py
+++ logAnalysis.py
@@ -13,8 +13,40 @@ def test():
   return c.fetchall()
   db.close()
 
-def interactive():
+def interactiveHello():
   intro = raw_input("What's your name? ")
   print("Hello %s lets explore the world!" % intro)
+
+def interactive():
+  # displaying options to the user
+  print(30 * '-')
+  print('   Report Options')
+  print(30 * '-')
+  print('1. Three most popular articles of all time')
+  print('2. Most popular authors of all time')
+  print('3. Days where more than 1% of requests lead to errors')
+  print(30 * '-')
+
+  answer = raw_input('Which report would you like to see: ')
+  try:
+    answer = int(answer)
+  except:
+    print(30 * '-')
+    print('Invalid option please enter a valid number')
+    print(30 * '-')
+    return interactive()
+
+
+  if answer == 1:
+    print('1. let me get you those.....')
+  elif answer == 2:
+    print('2. let me get you those.....')
+  elif answer == 3:
+    print('3. let me get you those.....')
+  else:
+    print(30 * '-')
+    print('Invalid number try again')
+    print(30 * '-')
+    return interactive()
 
 interactive()
